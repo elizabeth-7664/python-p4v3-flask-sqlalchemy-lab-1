e7f1acb887174c89321ec3d3f32052439cf07653
elizabeth ndinda <elizabethndinda41@gmail.com>
2025-06-14 12:08:05 +0300

    Use Flask-SQLAlchemy to define a data model

--- server/testing/app_magnitude_test.py
+++ server/testing/app_magnitude_test.py
@@ -8,48 +8,43 @@ from app import app
 class TestApp:
     '''Flask application in flask_app.py'''
 
-    def test_earthquake_magnitude_route(self):
-        '''has a resource available at "/earthquakes/magnitude/<magnitude>".'''
-        response = app.test_client().get('/earthquakes/magnitude/8.0')
+    def test_earthquake_found_route(self):
+        '''has a resource available at "/earthquakes/<id>".'''
+        response = app.test_client().get('/earthquakes/1')
         assert response.status_code == 200
 
-    def test_earthquakes_magnitude_match_response(self):
-        '''displays json in earthquake/magnitude route with keys for count, quakes'''
+    def test_earthquake_not_found_route(self):
+        '''has a resource available at "/earthquakes/<id>".'''
+        response = app.test_client().get('/earthquakes/999')
+        assert response.status_code == 404
 
-        response = app.test_client().get('/earthquakes/magnitude/9.0')
+    def test_earthquakes_found_response(self):
+        '''displays json in earthquake route with keys for id, magnitude, location, year'''
+
+        response = app.test_client().get('/earthquakes/2')
         # get the response body
         response_body = response.data.decode()
         # convert to JSON
         response_json = json.loads(response_body)
         # confirm JSON data
-        assert response_json["count"] == 2
-        assert len(response_json["quakes"]) == 2
-        # confirm list contents
-        quake1 = response_json["quakes"][0]
-        assert quake1["id"] == 1
-        assert quake1["magnitude"] == 9.5
-        assert quake1["location"] == "Chile"
-        assert quake1["year"] == 1960
-        quake2 = response_json["quakes"][1]
-        assert quake2["id"] == 2
-        assert quake2["magnitude"] == 9.2
-        assert quake2["location"] == "Alaska"
-        assert quake2["year"] == 1964
+        assert response_json["id"] == 2
+        assert response_json["magnitude"] == 9.2
+        assert response_json["location"] == "Alaska"
+        assert response_json["year"] == 1964
 
         # confirm status
         assert response.status_code == 200
 
-    def test_earthquakes_magnitude_no_match_response(self):
-        '''displays json in earthquake/magnitude route with keys for count, quakes'''
+    def test_earthquakes_not_found_response(self):
+        '''displays appropriate message if id not found'''
 
-        response = app.test_client().get('/earthquakes/magnitude/10.0')
+        response = app.test_client().get('/earthquakes/9999')
         # get the response body
         response_body = response.data.decode()
         # convert to JSON
         response_json = json.loads(response_body)
         # confirm JSON data
-        assert response_json["count"] == 0
-        assert len(response_json["quakes"]) == 0
+        assert response_json["message"] == "Earthquake 9999 not found."
 
         # confirm status
-        assert response.status_code == 200
+        assert response.status_code == 404
